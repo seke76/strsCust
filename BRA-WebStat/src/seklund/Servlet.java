e11e63102ff156a8fdf02aa69241038a5a303def
Stefan Eklund <stefan.eklund76@gmail.com>
2014-06-27 00:37:50 +0200

    db connection is now working. starting with jsp view.

--- BRA-WebStat/src/seklund/Servlet.java
+++ BRA-WebStat/src/seklund/Servlet.java
@@ -3,6 +3,7 @@ package seklund;
 import java.io.IOException;
 import java.sql.*;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -20,6 +21,7 @@ public class Servlet extends HttpServlet {
 	//private DataSource dataSource;
 	private Connection connection;
 	//private Statement statement;
+	String url = "jdbc:mysql://localhost:3306/strstest";		
 
 	public Servlet() {
 		super();
@@ -31,40 +33,57 @@ public class Servlet extends HttpServlet {
 
 		this.myParam = config.getInitParameter("myParam");
 
+
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection(url,"root", "");
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+
+
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		System.out.println("doGet");
 
-	
-			
-	
-		String url =
-				"jdbc:mysql://localhost:3306/strstest";
+		ResultSet rs;
+
+
 
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			connection = DriverManager.getConnection(url,"strs", "streamserve");
-		} catch (SQLException | ClassNotFoundException e) {
-			e.printStackTrace();
-		}
+			System.out.println("URL: " + url);
+			System.out.println("Connection: " + connection);
 
-		try {
 			Statement statement = connection.createStatement();
 			//statement.executeUpdate("CREATE TABLE myTable(test_id int," +
-				//	"test_val char(15) not null)");
-			connection.close();
+			//		"test_val char(15) not null)");
 
+			rs = statement.executeQuery("SELECT * " +
+					"from cows ORDER BY age");
+
+			System.out.println("Display all results:");
+
+			while(rs.next()){
+				String cowName= rs.getString("name");
+				int cowAge = rs.getInt("age");
+				System.out.println("\tCow= " + cowName
+						+ "\tage = " + cowAge);
+			}//end while loop
+
+			connection.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 
-
-
-		response.getWriter().write("<html><body>myParam = " +
+		/*response.getWriter().write("<html><body>myParam = " +
 				this.myParam + "</body></html>");
-
+*/
+		
+		RequestDispatcher view = request.getRequestDispatcher("/jsp/index.jsp");
+	    view.forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
