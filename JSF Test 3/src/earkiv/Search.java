bd8e7fc0cd79831d2afd1f8bfc453bd523a80af3
seklund <seklund@SE07334.opentext.net>
2015-05-23 08:04:49 +0200

    ...zzz

---
+++ JSF Test 3/src/earkiv/Search.java
@@ -0,0 +1,29 @@
+package earkiv;
+
+import javax.annotation.ManagedBean;
+
+@ManagedBean
+public class Search {
+
+	private String trackerId;
+	private String bgcId;
+	
+	public Search(String trackerId, String bgcId) {
+		System.out.println("Search constructur");
+		this.trackerId = trackerId;
+		this.bgcId = bgcId;
+	}
+	
+	public String getTrackerId() {
+		return trackerId;
+	}
+	public void setTrackerId(String trackerId) {
+		this.trackerId = trackerId;
+	}
+	public String getBgcId() {
+		return bgcId;
+	}
+	public void setBgcId(String bgcId) {
+		this.bgcId = bgcId;
+	}
+}
