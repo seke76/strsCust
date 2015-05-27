934e66a503355d0dfe27517a252c89e20d1e78bb
Stefan Eklund <stefan.eklund76@gmail.com>
2015-05-27 23:05:10 +0200

    2000

--- JSF Test 3/src/hello/Hello.java
+++ JSF Test 3/src/hello/Hello.java
@@ -5,7 +5,7 @@ import javax.annotation.ManagedBean;
 @ManagedBean
 public class Hello {
 
-    final String world = "Hello barn";
+    final String world = "Hello barn 200";
 
     public String getworld() {
         return world;
